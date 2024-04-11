bbb742f0234bd7eac3ab0227e21d97f56970cef6
scottyboii <scott.butler42@gmail.com>
2024-04-11 22:30:56 +0100

    Create the TaskRoomDatabase

---
+++ app/src/main/java/com/example/todolistjava/TaskRoomDatabase.java
@@ -0,0 +1,34 @@
+package com.example.todolistjava;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Task.class}, version = 1, exportSchema = false)
+public abstract class TaskRoomDatabase extends RoomDatabase {
+
+    public abstract TaskDao taskDao();
+
+    private static volatile TaskRoomDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static TaskRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (TaskRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    TaskRoomDatabase.class, "task_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
