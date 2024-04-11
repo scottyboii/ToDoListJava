146c3d3826afe3e0b78295301d36714465b45a8e
scottyboii <scott.butler42@gmail.com>
2024-04-11 22:33:59 +0100

    Create TaskRepository

---
+++ app/src/main/java/com/example/todolistjava/TaskRepository.java
@@ -0,0 +1,30 @@
+package com.example.todolistjava;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+class TaskRepository {
+
+    private TaskDao mTaskDao;
+    private LiveData<List<Task>> mAllTasks;
+
+    TaskRepository(Application application) {
+        TaskRoomDatabase db = TaskRoomDatabase.getDatabase(application);
+        mTaskDao = db.taskDao();
+        mAllTasks = mTaskDao.getAlphabetisedTasks();
+    }
+
+    LiveData<List<Task>> getAllTasks() {
+        return mAllTasks;
+    }
+
+    void inset(Task task) {
+        TaskRoomDatabase.databaseWriteExecutor.execute(() -> {
+            mTaskDao.insert(task);
+        });
+    }
+
+}
