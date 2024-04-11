66af82ffa45e4dae8ccba92f9dd457497817dba6
scottyboii <scott.butler42@gmail.com>
2024-04-11 22:36:53 +0100

    Create TaskViewModel

---
+++ app/src/main/java/com/example/todolistjava/TaskViewModel.java
@@ -0,0 +1,26 @@
+package com.example.todolistjava;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class TaskViewModel extends AndroidViewModel {
+
+    private TaskRepository mRepository;
+
+    private final LiveData<List<Task>> mAllTasks;
+
+    public TaskViewModel (Application application) {
+        super(application);
+        mRepository = new TaskRepository(application);
+        mAllTasks = mRepository.getAllTasks();
+    }
+
+    LiveData<List<Task>> getAllTasks() {return mAllTasks; }
+
+    public void insert(Task task) {mRepository.insert(task);}
+
+}
